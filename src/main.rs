23bd2b22b9f59002aec232f62a73dc24b6d64cfb
Alexander van Saase <avsaase@gmail.com>
2023-11-02 23:52:04 +0100

    Implement button controlled blinking

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,52 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::sync::atomic::{self, AtomicU32};
+
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    gpio::{Level, Output, Speed},
-    peripherals::PE8,
+    exti::ExtiInput,
+    gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed},
 };
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
+static BLINK_MS: AtomicU32 = AtomicU32::new(2000);
+
 #[embassy_executor::task]
-async fn blinker(mut led: Output<'static, PE8>, interval: Duration) {
+async fn led_task(led: AnyPin) {
+    let mut led = Output::new(led, Level::Low, Speed::Low);
     loop {
-        led.set_high();
-        info!("High");
-        Timer::after(interval).await;
-        info!("Low");
-        led.set_low();
-        Timer::after(interval).await;
+        let delay = BLINK_MS.load(atomic::Ordering::Relaxed);
+        led.toggle();
+        Timer::after(Duration::from_millis(delay.into())).await;
+        info!(
+            "Led turned {}",
+            if led.is_set_high() { "on" } else { "off" }
+        );
     }
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    let led = Output::new(p.PE8, Level::Low, Speed::Medium);
-    spawner
-        .spawn(blinker(led, Duration::from_millis(250)))
-        .unwrap();
+
+    let button = Input::new(p.PA0, Pull::None);
+    let mut button = ExtiInput::new(button, p.EXTI0);
+
+    let mut delay = BLINK_MS.load(atomic::Ordering::Relaxed);
+
+    spawner.spawn(led_task(p.PE8.degrade())).unwrap();
+
+    loop {
+        button.wait_for_rising_edge().await;
+        delay -= 200;
+        info!("Delay set to {}ms", delay);
+        if delay < 200 {
+            delay = 2000;
+            info!("Delay reset to {}ms", delay);
+        }
+        BLINK_MS.store(delay, atomic::Ordering::Relaxed);
+    }
 }
